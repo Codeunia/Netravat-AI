97a3149f4b98462317342bf3245e7c986b867d14
StackOverflowed18 <anshimasinghask@gmail.com>
2025-07-19 19:44:00 +0530

    added sign in ,signup in app auth

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        
+      
+    </div>
+  )
+}
+
+export default page
+// rafce is used to ensure that the component is loaded only on the client side
